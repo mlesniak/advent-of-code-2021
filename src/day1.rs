9e8c7416c68ccda75e7fd35c840e95ed37e6d90a
Michael Lesniak <mail@mlesniak.com>
2021-12-01 06:55:26 +0100

    Day 1 -- solution for part 2

--- src/day1.rs
+++ src/day1.rs
@@ -3,15 +3,36 @@ use std::io::Read;
 
 pub fn day1() {
     let s = readFile("day1.txt");
-
     let lines: Vec<&str> = s.split("\n").collect();
-
     let mut numbers: Vec<i32> = Vec::new();
     for line in lines {
         let x: i32 = line.parse().unwrap();
         numbers.push(x);
     }
 
+    //  part1(&mut numbers);
+    part2(&mut numbers);
+}
+
+fn part2(numbers: &Vec<i32>) {
+    let mut increase = 0;
+
+    let mut windows: Vec<i32> = Vec::new();
+    for i in 0..(numbers.len() - 2) {
+        let sum = numbers[i] + numbers[i + 1] + numbers[i + 2];
+        windows.push(sum);
+    }
+
+    for i in 1..(windows.len()) {
+        if windows[i] > windows[i - 1] {
+            increase += 1;
+        }
+    }
+
+    println!("{}", increase);
+}
+
+fn part1(numbers: &Vec<i32>) {
     let mut increase = 0;
     for i in 1..(numbers.len()) {
         if numbers[i] > numbers[i - 1] {
