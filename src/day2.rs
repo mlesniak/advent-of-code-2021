88cb7fb298e779c1e80fb7394012a0a54bd14976
Michael Lesniak <mail@mlesniak.com>
2021-12-02 06:47:37 +0100

    Day 2 part 2 testcase working

--- src/day2.rs
+++ src/day2.rs
@@ -6,6 +6,36 @@ pub enum Part {
     Part2,
 }
 
+pub fn part2() {
+    let s = read_file("day2.txt");
+    let lines: Vec<_> = s.split("\n").collect();
+
+    let mut aim = 0;
+    let mut x = 0;
+    let mut y = 0;
+
+    for line in lines {
+        let parts: Vec<_> = line.split_ascii_whitespace().collect();
+        println!("{:?}", parts);
+
+        let arg: u32 = parts[1].parse().unwrap();
+
+        match parts[0] {
+            "forward" => {
+                x = x + arg;
+                y = y + arg * aim;
+            },
+            "down" => aim = aim + arg,
+            "up" => aim = aim - arg,
+            _ => panic!("not found: {}", parts[0])
+        }
+    }
+
+    let res = x * y;
+    println!("{}", res);
+
+}
+
 pub fn part1() {
     let s = read_file("day2.txt");
     let lines: Vec<_> = s.split("\n").collect();
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     // day1::day1(day1::Part::Part1);
     // day1::day1(day1::Part::Part2);
 
-    day2::part1();
+    // day2::part1();
+    day2::part2();
 }
 
