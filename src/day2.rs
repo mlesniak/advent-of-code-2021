9ec95c040ef6854d4a3449d00b5a41e3e8444887
Michael Lesniak <mail@mlesniak.com>
2021-12-02 06:42:06 +0100

    Day 2 testcase working

---
+++ src/day2.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use std::io::Read;
+
+pub enum Part {
+    Part1,
+    Part2,
+}
+
+pub fn part1() {
+    let s = read_file("day2.txt");
+    let lines: Vec<_> = s.split("\n").collect();
+
+    let mut x = 0;
+    let mut y = 0;
+
+    for line in lines {
+        let parts: Vec<_> = line.split_ascii_whitespace().collect();
+        println!("{:?}", parts);
+
+        let arg: u32 = parts[1].parse().unwrap();
+
+        match parts[0] {
+            "forward" => x = x + arg,
+            "down" => y = y + arg,
+            "up" => y = y - arg,
+            _ => panic!("not found: {}", parts[0])
+        }
+    }
+
+    let res = x * y;
+    println!("{}", res);
+}
+
+fn read_file(path: &str) -> String {
+    let mut f = File::open(path).unwrap();
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => s,
+        Err(_) => panic!("Unable to read input file")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod day1;
+mod day2;
 
 fn main() {
-    day1::day1(day1::Part::Part1);
-    day1::day1(day1::Part::Part2);
+    // day1::day1(day1::Part::Part1);
+    // day1::day1(day1::Part::Part2);
+
+    day2::part1();
 }
 
