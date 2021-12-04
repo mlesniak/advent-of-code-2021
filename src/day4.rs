c0650dbd200b13f8f6fa26048868ba571edebceb
Michael Lesniak <mail@mlesniak.com>
2021-12-04 09:07:59 +0100

    Day 4 Mark called numbers

--- src/day4.rs
+++ src/day4.rs
@@ -1,23 +1,40 @@
 use crate::util::read_lines;
 
 #[derive(Debug)]
+struct BoardNumber {
+    number: i32,
+    marked: bool,
+}
+
+#[derive(Debug)]
 struct Board {
-    board: Vec<Vec<i32>>,
+    board: Vec<Vec<BoardNumber>>,
 }
 
 impl Board {
     fn read(lines: &Vec<String>, start: usize) -> Board {
-        let mut board: Vec<Vec<i32>> = Vec::new();
+        let mut board: Vec<Vec<BoardNumber>> = Vec::new();
         for i in start..(start + 5) {
-            let values: Vec<i32> = lines[i]
+            let values: Vec<BoardNumber> = lines[i]
                 .split(" ")
                 .filter(|x| x.len() > 0)
                 .map(|x| x.parse().unwrap())
+                .map(|x| BoardNumber { number: x, marked: false })
                 .collect();
             board.push(values);
         }
 
-        Board {board }
+        Board { board }
+    }
+
+    fn mark(mut self: &mut Board, number: i32) {
+        for row in &mut self.board {
+            for bn in row {
+                if bn.number == number {
+                    (*bn).marked = true
+                }
+            }
+        }
     }
 }
 
@@ -25,7 +42,7 @@ pub fn part1() {
     let lines = read_lines("day4.txt");
 
     let numbers: Vec<i32> = lines[0].split(",").map(|x| x.parse().unwrap()).collect();
-    println!("{:?}", numbers);
+    // println!("{:?}", numbers);
 
     let num_boards = (lines.len() - 2) / 5;
     let mut boards: Vec<Board> = Vec::new();
@@ -33,7 +50,18 @@ pub fn part1() {
         let b = Board::read(&lines, start);
         boards.push(b);
     }
-    println!("{:?}", boards);
+    // println!("{:?}", boards);
+
+    for number in numbers {
+        println!("{}", number);
+        for board in &mut boards {
+            board.mark(number);
+        }
+
+        for b in &boards {
+            println!("{:?}", b);
+        }
+    }
 }
 
 pub fn part2() {}
