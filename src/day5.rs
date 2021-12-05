5ac8623522a6183c4dba0d795f4c0d95780297f1
Michael Lesniak <mail@mlesniak.com>
2021-12-05 09:54:56 +0100

    Day 5 Filter for straight lines

--- src/day5.rs
+++ src/day5.rs
@@ -8,7 +8,11 @@ struct Point {
 
 impl Point {
     fn parse(coord: &str) -> Point {
-        let numbers: Vec<_> = coord.split(",").map(|x| x.parse().unwrap()).collect();
+        let numbers: Vec<_> = coord
+            .trim()
+            .split(",")
+            .map(|x| x.parse().unwrap())
+            .collect();
         Point {
             x: numbers[0],
             y: numbers[1],
@@ -36,8 +40,18 @@ pub fn part1() {
     let coordinates = read_lines("day5.txt");
     println!("{:?}", coordinates);
 
+    let mut lines = Vec::new();
     for c in coordinates {
         let s = Segment::parse(&c);
-        println!("{:?}", s);
+        // For now, only consider horizontal and vertical lines:
+        // lines where either x1 = x2 or y1 = y2.
+        if s.start.x == s.end.x || s.start.y == s.end.y {
+            lines.push(s);
+        }
     }
+
+    for line in lines {
+        println!("{:?}", line);
+    }
+        
 }
