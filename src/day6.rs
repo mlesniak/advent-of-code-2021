f3d19568507b5e057fc50c2ffebb18f99e2bee5b
Michael Lesniak <mail@mlesniak.com>
2021-12-06 07:01:56 +0100

    Day 6 Part 1 Second testcase working

--- src/day6.rs
+++ src/day6.rs
@@ -1,7 +1,7 @@
 use crate::util::read_file;
 
 pub fn part1() {
-    let max_days = 18;
+    let max_days = 80;
 
     let mut fishes: Vec<i32> = read_file("day6.txt").split(",").map(|x| x.parse().unwrap()).collect();
     // println!("{:?}", fishes);
@@ -9,7 +9,7 @@ pub fn part1() {
     let mut new_fish: Vec<i32> = Vec::new();
     for day in 0..max_days {
         println!("{}", day);
-        println!("{}: {:?}", day, &fishes);
+        // println!("{}: {:?}", day, &fishes);
         for fish in &mut fishes {
             if *fish == 0 {
                 *fish = 6 + 1;
