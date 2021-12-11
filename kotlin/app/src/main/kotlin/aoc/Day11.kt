8de109e2e8d0eb0f6c81a47ea6da669f647eff79
Michael Lesniak <mail@mlesniak.com>
2021-12-11 10:06:41 +0100

    [Day11] Generics

--- kotlin/app/src/main/kotlin/aoc/Day11.kt
+++ kotlin/app/src/main/kotlin/aoc/Day11.kt
@@ -1,42 +1,18 @@
 package aoc
 
-import java.io.File
-
 class Day11 {
     fun part1() {
-        val grid = readGrid("day11.txt")
-        grid.debug()
+        val current = readGrid("day11.txt")
+        current.debug()
 
-        val c = grid.copy()
-        grid[2][3] = -100
-        grid.debug()
-        c.debug()
+        // Increase
+        current.forEach { x, y, v ->
+            v+1
+        }
+        println()
+        current.debug()
+
+
     }
 }
 
-typealias Grid = Array<Array<Int>>
-
-fun Grid.debug() {
-    forEach { row ->
-        println(row.contentToString())
-    }
-}
-
-fun Grid.copy(): Grid {
-    val rows = mutableListOf<Array<Int>>()
-    forEach { row ->
-        val copy  = row.copyOf()
-        rows.add(copy)
-    }
-
-    return rows.toTypedArray()
-}
-
-fun readGrid(filename: String): Grid {
-    val rows = mutableListOf<Array<Int>>()
-    File(filename).readLines().forEach { line ->
-        val cols = line.map { c -> c.toString().toInt() }.toTypedArray()
-        rows.add(cols)
-    }
-    return rows.toTypedArray()
-}
---
+++ kotlin/app/src/main/kotlin/aoc/Grid.kt
@@ -0,0 +1,39 @@
+package aoc
+
+import java.io.File
+
+typealias Grid<T> = Array<Array<T>>
+
+fun <T> Grid<T>.debug() {
+    forEach { row ->
+        println(row.contentToString())
+    }
+}
+
+inline fun <reified T> Grid<T>.copy(): Grid<T> {
+    val rows = mutableListOf<Array<T>>()
+    forEach { row ->
+        val copy = row.copyOf()
+        rows.add(copy)
+    }
+
+    return rows.toTypedArray()
+}
+
+fun readGrid(filename: String): Grid<Int> {
+    val rows = mutableListOf<Array<Int>>()
+    File(filename).readLines().forEach { line ->
+        val cols = line.map { c -> c.toString().toInt() }.toTypedArray()
+        rows.add(cols)
+    }
+    return rows.toTypedArray()
+}
+
+fun <T> Grid<T>.forEach(f: (x: Int, y: Int, v: T) -> T) {
+    indices.forEach { y ->
+        get(y).indices.forEach { x ->
+            val v = get(y)[x]
+            get(y)[x] = f(x, y, v)
+        }
+    }
+}
