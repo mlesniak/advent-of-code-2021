65216ae0fa5e5b2a62bbcef959eb09e681f707fd
Michael Lesniak <mail@mlesniak.com>
2021-12-11 09:46:23 +0100

    [Day11] Copy grid

--- kotlin/app/src/main/kotlin/aoc/Day11.kt
+++ kotlin/app/src/main/kotlin/aoc/Day11.kt
@@ -6,17 +6,33 @@ class Day11 {
     fun part1() {
         val grid = readGrid("day11.txt")
         grid.debug()
-    }
 
+        val c = grid.copy()
+        grid[2][3] = -100
+        grid.debug()
+        c.debug()
+    }
 }
 
-fun Array<Array<Int>>.debug() {
+typealias Grid = Array<Array<Int>>
+
+fun Grid.debug() {
     forEach { row ->
         println(row.contentToString())
     }
 }
 
-fun readGrid(filename: String): Array<Array<Int>> {
+fun Grid.copy(): Grid {
+    val rows = mutableListOf<Array<Int>>()
+    forEach { row ->
+        val copy  = row.copyOf()
+        rows.add(copy)
+    }
+
+    return rows.toTypedArray()
+}
+
+fun readGrid(filename: String): Grid {
     val rows = mutableListOf<Array<Int>>()
     File(filename).readLines().forEach { line ->
         val cols = line.map { c -> c.toString().toInt() }.toTypedArray()
