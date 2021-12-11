343775101ef5820353844fba75a29d7fbb00c6db
Michael Lesniak <mail@mlesniak.com>
2021-12-11 17:48:25 +0100

    [Day11] Minimal example working

--- kotlin/app/src/main/kotlin/aoc/Day11.kt
+++ kotlin/app/src/main/kotlin/aoc/Day11.kt
@@ -2,17 +2,29 @@ package aoc
 
 class Day11 {
     fun part1() {
-        val current = readGrid("day11.txt") { it }
+        var current = readGrid("day11.txt") { it }
+        val max_step = 3
+
+        println("Before any steps:")
         current.debug()
 
-        // Increase
-        current.forEach { x, y, v ->
-            v+1
+        for (step in 1..max_step) {
+            val next = current.map { x, y, v ->
+                // println("$x,$y <- $v")
+                var flush = 0
+                current.neighbors(x, y) { nx, ny, v ->
+                    // println("  $nx, $ny: $v")
+                    if (v >= 9) {
+                        flush++
+                    }
+                }
+                if (v >= 9) 0 else (v + 1 + flush) % 10
+            }
+
+            println("After step $step:")
+            next.debug()
+            current = next
         }
-        println()
-        current.debug()
-
-
     }
 }
 
--- kotlin/app/src/main/kotlin/aoc/Grid.kt
+++ kotlin/app/src/main/kotlin/aoc/Grid.kt
@@ -2,11 +2,13 @@ package aoc
 
 import java.io.File
 
+data class Point(val x: Int, val y: Int)
+
 typealias Grid<T> = Array<Array<T>>
 
 fun <T> Grid<T>.debug() {
     forEach { row ->
-        println(row.contentToString())
+        println(row.joinToString(""))
     }
 }
 
@@ -39,6 +41,42 @@ fun <T> Grid<T>.forEach(f: (x: Int, y: Int, v: T) -> T) {
         get(y).indices.forEach { x ->
             val v = get(y)[x]
             get(y)[x] = f(x, y, v)
+        }
+    }
+}
+
+inline fun <reified T> Grid<T>.map(f: (x: Int, y: Int, v: T) -> T): Grid<T> {
+    val rows = mutableListOf<Array<T>>()
+
+    indices.forEach { y ->
+        val cols = get(y)
+        val newCols = cols.copyOf()
+        cols.indices.forEach { x ->
+            val v = cols[x]
+            newCols[x] = f(x, y, v)
+        }
+        rows.add(newCols)
+    }
+
+    return rows.toTypedArray()
+}
+
+fun <T> Grid<T>.neighbors(x: Int, y:Int, f: (x: Int, y: Int, value: T) -> Unit) {
+    for (dy in -1..1) {
+        for (dx in -1..1) {
+            val nx = x + dx
+            val ny = y + dy
+            if (dx== 0 && dy == 0) {
+                continue
+            }
+            if (nx < 0 || ny < 0) {
+                continue
+            }
+            if (nx >= get(0).size || ny >= size) {
+                continue
+            }
+
+            f(nx, ny, get(ny)[nx])
         }
     }
 }
