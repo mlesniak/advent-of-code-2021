430e40118393696d21403c4bfe9c280a6330b9b5
Michael Lesniak <mail@mlesniak.com>
2021-12-11 09:40:41 +0100

    [Day11] Refactoring

--- kotlin/app/src/main/kotlin/aoc/Day11.kt
+++ kotlin/app/src/main/kotlin/aoc/Day11.kt
@@ -4,20 +4,23 @@ import java.io.File
 
 class Day11 {
     fun part1() {
-        val rows = mutableListOf<Array<Int>>()
-        File("day11.txt").readLines().forEach { line ->
-            val cols = line.map { c -> c.toString().toInt() }.toTypedArray()
-            rows.add(cols)
-            // println(cols.contentToString())
-        }
-        val grid = rows.toTypedArray()
-
+        val grid = readGrid("day11.txt")
         grid.debug()
     }
+
 }
 
 fun Array<Array<Int>>.debug() {
     forEach { row ->
         println(row.contentToString())
     }
+}
+
+fun readGrid(filename: String): Array<Array<Int>> {
+    val rows = mutableListOf<Array<Int>>()
+    File(filename).readLines().forEach { line ->
+        val cols = line.map { c -> c.toString().toInt() }.toTypedArray()
+        rows.add(cols)
+    }
+    return rows.toTypedArray()
 }
