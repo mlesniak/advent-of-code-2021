394e6fb28b0c5ef6f82443379840855bb78bb9fd
Michael Lesniak <mail@mlesniak.com>
2021-12-14 06:28:39 +0100

    [Day14] Working test case

--- kotlin/app/src/main/kotlin/aoc/Day14.kt
+++ kotlin/app/src/main/kotlin/aoc/Day14.kt
@@ -11,7 +11,32 @@ class Day14 {
             val parts = input[lineIdx].split(" -> ")
             rules[parts[0]] = parts[1]
         }
-        start.debug()
+        // start.debug()
         rules.debug()
+
+        val maxSteps = 4
+        var state = start
+        println()
+        state.debug()
+        for (step in 1..maxSteps) {
+            println("=== Step $step")
+            state = step(rules, state)
+            state.debug()
+        }
+
+        println("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")
+    }
+
+    private fun step(rules: Map<String, String>, state: String): String {
+        val sb = StringBuilder()
+        for (i in 0..state.length-2) {
+            val pattern = state.substring(i, i+2)
+            // pattern.debug()
+            val match = rules[pattern]
+            sb.append("${pattern[0]}$match")
+            // sb.insert(i+1, match)
+        }
+        sb.append(state[state.length-1])
+        return sb.toString()
     }
 }
