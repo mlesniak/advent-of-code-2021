e0eab4798feffd34b2e7ae80f683abe9558dfd7f
Michael Lesniak <mail@mlesniak.com>
2021-12-15 18:50:46 +0100

    [Day15] Greedy does not work as well

--- kotlin/app/src/main/kotlin/aoc/Day15.kt
+++ kotlin/app/src/main/kotlin/aoc/Day15.kt
@@ -1,7 +1,5 @@
 package aoc
 
-import kotlin.random.Random
-
 class Day15 {
     data class Path(val path: List<Point>, val risk: Int) {
         val points: Set<Point> = path.toSet()
@@ -32,70 +30,104 @@ class Day15 {
 
         val goal = Point(map.width() - 1, map.height() - 1)
 
-        val rand = Random(0)
-
-        while (paths.isNotEmpty()) {
-            // println("\n\nIteration")
-            // paths.forEach { println("  * $it") }
-
-            val current = paths.removeAt(paths.size - 1)
-            if (rand.nextDouble() < 0.001) {
-                val avg = paths.map { it.risk }.sum() / paths.size
-                println("${paths.size} and minRisk=$minRisk, avg=$avg")
-                // println("${paths.map{it.risk}}")
+        // Greedy algorithm.
+        var current = start
+        while (true) {
+            val tip = current.path.last()
+            if (tip == goal) {
+                break
             }
 
-            val last = current.path.last()
-
-            // println("= Examining $current")
-            val nexts = mutableListOf<Path>()
-            map.neighbors(last.x, last.y) { nx, ny, risk ->
-                if (Point(nx, ny) == goal) {
-                    println("*** Found path $current.risk")
-
-                    // Prune tree
-                    minRisk = current.risk + risk
-                    val before = paths.size
-                    paths.removeIf { it.risk >= minRisk }
-                    println("- Pruned ${before - paths.size}")
+            var nextPoint = Point(0,0)
+            var minRisk = Int.MAX_VALUE
+            map.neighbors(tip.x, tip.y) { nx, ny, risk ->
+                if (Point(nx, ny) in current.path) {
                     return@neighbors
                 }
-
-
-                if (current.risk + risk >= minRisk) {
-                    return@neighbors
+                if (risk < minRisk) {
+                    nextPoint = Point(nx, ny)
+                    minRisk = risk
                 }
-
-                val next = Point(nx, ny)
-                if (current.contains(next)) {
-                    return@neighbors
-                }
-                // println("-> $nx/$ny with $risk")
-                val nextPath = Path(
-                    current.path + next,
-                    current.risk + risk
-                )
-                nexts += nextPath
             }
-            if (nexts.isEmpty()) {
-                continue
+            if (minRisk == Int.MAX_VALUE) {
+                println("Np path for...")
+                current.path.forEach { println("  $it") }
+                throw IllegalStateException("no path continuation")
             }
 
-            // Sort by lowest risk
-            nexts.sortBy { it.risk }
-            nexts.reverse()
-            // println("Choosing nexts:")
-            // nexts.forEach { println("  $it") }
-            paths += nexts
-
-            paths.sortBy {
-                it.path.size / it.risk
-            }
-            paths.reverse()
-
-            // readLine()
+            current = Path(
+                path = current.path + nextPoint,
+                risk = current.risk + minRisk
+            )
         }
 
-        println("minRisk=$minRisk")
+        println(current)
+
+
+        // val rand = Random(0)
+        //
+        // while (paths.isNotEmpty()) {
+        //     // println("\n\nIteration")
+        //     // paths.forEach { println("  * $it") }
+        //
+        //     val current = paths.removeAt(paths.size - 1)
+        //     if (rand.nextDouble() < 0.001) {
+        //         val avg = paths.map { it.risk }.sum() / paths.size
+        //         println("${paths.size} and minRisk=$minRisk, avg=$avg")
+        //         // println("${paths.map{it.risk}}")
+        //     }
+        //
+        //     val last = current.path.last()
+        //
+        //     // println("= Examining $current")
+        //     val nexts = mutableListOf<Path>()
+        //     map.neighbors(last.x, last.y) { nx, ny, risk ->
+        //         if (Point(nx, ny) == goal) {
+        //             println("*** Found path $current.risk")
+        //
+        //             // Prune tree
+        //             minRisk = current.risk + risk
+        //             val before = paths.size
+        //             paths.removeIf { it.risk >= minRisk }
+        //             println("- Pruned ${before - paths.size}")
+        //             return@neighbors
+        //         }
+        //
+        //
+        //         if (current.risk + risk >= minRisk) {
+        //             return@neighbors
+        //         }
+        //
+        //         val next = Point(nx, ny)
+        //         if (current.contains(next)) {
+        //             return@neighbors
+        //         }
+        //         // println("-> $nx/$ny with $risk")
+        //         val nextPath = Path(
+        //             current.path + next,
+        //             current.risk + risk
+        //         )
+        //         nexts += nextPath
+        //     }
+        //     if (nexts.isEmpty()) {
+        //         continue
+        //     }
+        //
+        //     // Sort by lowest risk
+        //     nexts.sortBy { it.risk }
+        //     nexts.reverse()
+        //     // println("Choosing nexts:")
+        //     // nexts.forEach { println("  $it") }
+        //     paths += nexts
+        //
+        //     paths.sortBy {
+        //         it.path.size / it.risk
+        //     }
+        //     paths.reverse()
+        //
+        //     // readLine()
+        // }
+        //
+        // println("minRisk=$minRisk")
     }
 }
