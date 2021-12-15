fb6aae7dbe6654512fcf7bf1a58cf4edbab729af
Michael Lesniak <mail@mlesniak.com>
2021-12-15 07:50:59 +0100

    [Day15] Statistics

--- kotlin/app/src/main/kotlin/aoc/Day15.kt
+++ kotlin/app/src/main/kotlin/aoc/Day15.kt
@@ -1,7 +1,13 @@
 package aoc
 
+import kotlin.random.Random
+
 class Day15 {
-    data class Path(val path: List<Point>, val risk: Int)
+    data class Path(val path: List<Point>, val risk: Int) {
+        val points: Set<Point> = path.toSet()
+
+        fun contains(p: Point): Boolean = p in points
+    }
 
     fun part1() {
         val map: Grid<Int> = readGrid("day15.txt")
@@ -15,10 +21,26 @@ class Day15 {
         paths += start
 
         var minRisk = Int.MAX_VALUE
+        var sum = 0
+        for (x in 0 until map.width()) {
+            sum += map[0][x]
+        }
+        for (y in 0 until map.height()) {
+            sum += map[y][map.width() - 1]
+        }
+        minRisk = sum
+
         val goal = Point(map.width() - 1, map.height() - 1)
+
+        val rand = Random(0)
 
         while (paths.isNotEmpty()) {
             val current = paths.removeAt(paths.size - 1)
+            if (rand.nextDouble() < 0.001) {
+                val avg = paths.map { it.risk }.sum() / paths.size
+                println("${paths.size} and minRisk=$minRisk, avg=$avg")
+                println("${paths.map{it.risk}}")
+            }
 
             val last = current.path.last()
 
@@ -31,7 +53,7 @@ class Day15 {
                     // Prune tree
                     minRisk = current.risk + risk
                     val before = paths.size
-                    paths.removeIf { it.risk >= minRisk}
+                    paths.removeIf { it.risk >= minRisk }
                     println("- Pruned ${before - paths.size}")
                     return@neighbors
                 }
@@ -42,7 +64,7 @@ class Day15 {
                 }
 
                 val next = Point(nx, ny)
-                if (current.path.contains(next)) {
+                if (current.contains(next)) {
                     return@neighbors
                 }
                 // println("-> $nx/$ny with $risk")
@@ -57,6 +79,8 @@ class Day15 {
             nexts.sortBy { it.risk }
             nexts.reverse()
             paths += nexts
+
+
 
             // readLine()
         }
