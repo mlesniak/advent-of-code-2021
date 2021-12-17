8eba4c25c9aba7b437b90127290cfef8206d6429
Michael Lesniak <mail@mlesniak.com>
2021-12-17 19:55:19 +0100

    [Day17] Part 1 test example

--- kotlin/app/src/main/kotlin/aoc/Day17.kt
+++ kotlin/app/src/main/kotlin/aoc/Day17.kt
@@ -10,22 +10,43 @@ class Day17 {
         // val target = Area(144, -100, 178, -76)
         val target = Area(20, -10, 30, -5)
 
-        val start = Point(0, 0)
-        val velocity = Point(7, 2)
-        simulate(start, velocity, 10, target)
+        simulate(Point(0,0), Point(6,9), 50, target)
+
+        val hits = mutableListOf<Int>()
+        for (x in 0..20) {
+            for (y in 0..20) {
+                println("x=$x,y=$y")
+                val start = Point(0, 0)
+                val velocity = Point(x, y)
+                val hit = simulate(start, velocity, 50, target)
+                if (hit.hit) {
+                    hits += hit.maxY
+                }
+            }
+        }
+        println("$hits")
+        val maxY = hits.maxOrNull()
+        println("maxY=$maxY")
     }
 
-    private fun simulate(start: Point, velocity: Point, maxSteps: Int, target: Area): Boolean {
-        println("***** start=$start, velocity=$velocity")
+    data class Result(val hit: Boolean, val maxY: Int)
+
+    private fun simulate(start: Point, velocity: Point, maxSteps: Int, target: Area): Result {
+        // println("***** start=$start, velocity=$velocity")
         var vel = velocity
         var pos = start
+        var maxY = Int.MIN_VALUE
         for (step in 0 until maxSteps) {
-            println("\nStep $step")
+            // println("\nStep $step")
             pos += vel
+            if (pos.y > maxY) {
+                maxY = pos.y
+            }
+
             // println("pos=$pos")
             if (pos in target) {
-                println("Hit target after $step steps")
-                return true
+                // println("Hit target after $step steps with maxY=$maxY")
+                return Result(true, maxY)
             }
             val dx = if (vel.x > 0) {
                 -1
@@ -35,10 +56,11 @@ class Day17 {
                 0
             }
             val dy = -1
-            vel = Point(vel.x + dx, vel.y + dy)
-            println("  pos=$pos, vel=$vel")
+            val ny = vel.y + dy
+            vel = Point(vel.x + dx, ny)
+            // println("  pos=$pos, vel=$vel")
         }
 
-        return false
+        return Result(false, 0)
     }
 }
