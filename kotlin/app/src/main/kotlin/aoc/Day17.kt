c36dc11fd90ab54403f81931de6f3d50fab77f32
Michael Lesniak <mail@mlesniak.com>
2021-12-17 20:01:39 +0100

    [Day17] Part 2

--- kotlin/app/src/main/kotlin/aoc/Day17.kt
+++ kotlin/app/src/main/kotlin/aoc/Day17.kt
@@ -7,16 +7,16 @@ class Day17 {
     }
 
     fun part1() {
-        // val target = Area(144, -100, 178, -76)
-        val target = Area(20, -10, 30, -5)
+        val target = Area(144, -100, 178, -76)
+        // val target = Area(20, -10, 30, -5)
 
         simulate(Point(0,0), Point(6,9), 50, target)
 
         var counter = 0
         val hits = mutableListOf<Result>()
-        for (x in 0..100) {
+        for (x in 0..1000) {
             println("$x")
-            for (y in -100..100) {
+            for (y in -500..500) {
                 // println("x=$x,y=$y")
                 val start = Point(0, 0)
                 val velocity = Point(x, y)
