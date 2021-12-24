5074a12fd15a54c942664ba43fd6e42e23ca32c1
Michael Lesniak <mail@mlesniak.com>
2021-12-24 08:23:22 +0100

    [Day20] Part 1 test working

--- kotlin/app/src/main/kotlin/aoc/Day20.kt
+++ kotlin/app/src/main/kotlin/aoc/Day20.kt
@@ -8,7 +8,7 @@ class Day20 {
         val algorithm = file.next()
         file.next()
 
-        val pixels = mutableSetOf<Vector2>()
+        var pixels = mutableSetOf<Vector2>()
         var y = 0
         while (file.hasNext()) {
             val line = file.next()
@@ -21,6 +21,59 @@ class Day20 {
 
             y++
         }
-        pixels.debug()
+        pixels.render()
+
+        for (step in 1..2) {
+            separator()
+            println("STEP $step")
+            val next = mutableSetOf<Vector2>()
+            val x1 = pixels.minOf { it.x } - 1
+            val y1 = pixels.minOf { it.y } - 1
+            val x2 = pixels.maxOf { it.x } + 1
+            val y2 = pixels.maxOf { it.y } + 1
+            for (y in y1..y2) {
+                for (x in x1..x2) {
+                    println("\nExamining $x/$y")
+                    val sb = StringBuilder()
+
+                    for (dy in arrayOf(-1, 0, 1)) {
+                        for (dx in arrayOf(-1, 0, 1)) {
+                            val v = Vector2(x + dx, y + dy)
+                            // println("$v -> ${v in pixels}")
+                            if (v in pixels) {
+                                sb.append("1")
+                            } else {
+                                sb.append("0")
+                            }
+                        }
+                    }
+
+                    val index = sb.toString().toInt(2)
+                    if (algorithm[index] == '#') {
+                        next += Vector2(x, y)
+                    }
+                }
+            }
+            next.render()
+            pixels = next
+        }
+    }
+}
+
+fun Set<Vector2>.render() {
+    val x1 = minOf { it.x } - 1
+    val y1 = minOf { it.y } - 1
+    val x2 = maxOf { it.x } + 1
+    val y2 = maxOf { it.y } + 1
+
+    for (y in y1..y2) {
+        for (x in x1..x2) {
+            if (Vector2(x, y) in this) {
+                print("#")
+            } else {
+                print(".")
+            }
+        }
+        println()
     }
 }
