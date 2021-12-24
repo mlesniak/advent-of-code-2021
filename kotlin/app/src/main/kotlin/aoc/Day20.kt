8043eb4e572b01645b24ed563ca124a433d47c7e
Michael Lesniak <mail@mlesniak.com>
2021-12-24 07:50:11 +0100

    [Day20] Parse input file

--- kotlin/app/src/main/kotlin/aoc/Day20.kt
+++ kotlin/app/src/main/kotlin/aoc/Day20.kt
@@ -1,7 +1,26 @@
 package aoc
 
+import java.io.File
+
 class Day20 {
     fun part1() {
+        val file = File("day20.txt").readLines().iterator()
+        val algorithm = file.next()
+        file.next()
 
+        val pixels = mutableSetOf<Vector2>()
+        var y = 0
+        while (file.hasNext()) {
+            val line = file.next()
+            for (x in line.indices) {
+                val c = line[x]
+                if (c == '#') {
+                    pixels += Vector2(x, y)
+                }
+            }
+
+            y++
+        }
+        pixels.debug()
     }
 }
