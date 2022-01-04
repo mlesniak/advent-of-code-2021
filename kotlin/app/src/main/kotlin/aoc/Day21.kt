883b7e0bc0cf9d7de75ebb7dce8bd588d1ed582b
Michael Lesniak <mail@mlesniak.com>
2022-01-04 06:26:26 +0100

    [Day21] Show result

--- kotlin/app/src/main/kotlin/aoc/Day21.kt
+++ kotlin/app/src/main/kotlin/aoc/Day21.kt
@@ -20,7 +20,8 @@ class Day21 {
         root.debug("Initial state")
 
         val result = compute(root)
-        println("Part 2: $result")
+        val r = if (result.first > result.second) result.first else result.second
+        println("Part 2: $r")
     }
 
     // TODO(mlesniak) Add caching later
@@ -99,46 +100,4 @@ class Day21 {
             }
         }
     }
-
-    // fun part1() {
-    //     val players = arrayOf(
-    //         // Player(4),
-    //         // Player(8)
-    //
-    //         // Real input
-    //         Player(4),
-    //         Player(3)
-    //     )
-    //     var dice = 0
-    //     var counter = 0
-    //     var player = 0
-    //
-    //     while (true) {
-    //         separator()
-    //         println("Current player $player, dice rolled $counter")
-    //         val current = players[player]
-    //         repeat(3) {
-    //             counter++
-    //             dice = dice % 100 + 1
-    //             println("dice=$dice")
-    //             current.position += dice
-    //             println("position=${current.position}")
-    //         }
-    //         current.score += if (current.position % 10 == 0) {
-    //             10
-    //         } else {
-    //             current.position % 10
-    //         }
-    //         println("Score: ${current.score}")
-    //         player = (player + 1) % 2
-    //
-    //         if (current.score >= 1000) {
-    //             break
-    //         }
-    //     }
-    //
-    //     println("Points of losing player: ${players[player].score}")
-    //     println("Dice rolled $counter")
-    //     println("Part1 ${players[player].score * counter}")
-    // }
 }
