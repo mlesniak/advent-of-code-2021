f49671762eac41e69662017223fead245ae5d946
Michael Lesniak <mail@mlesniak.com>
2022-01-12 15:50:08 +0100

    [Day24] Interpreter ready

--- kotlin/app/src/main/kotlin/aoc/Day24.kt
+++ kotlin/app/src/main/kotlin/aoc/Day24.kt
@@ -10,9 +10,33 @@ class Day24 {
             }
         }
 
-        class Mul(val a: String, val b: Int) : Command() {
+        class Mul(val a: String, val b: String) : Command() {
             override fun toString(): String {
                 return "Mul(src='$a', value=$b)"
+            }
+        }
+
+        class Add(val a: String, val b: String) : Command() {
+            override fun toString(): String {
+                return "Add(a='$a', b='$b')"
+            }
+        }
+
+        class Div(val a: String, val b: String) : Command() {
+            override fun toString(): String {
+                return "Div(a='$a', b='$b')"
+            }
+        }
+
+        class Mod(val a: String, val b: String) : Command() {
+            override fun toString(): String {
+                return "Mod(a='$a', b='$b')"
+            }
+        }
+
+        class Eql(val a: String, val b: String) : Command() {
+            override fun toString(): String {
+                return "Eql(a='$a', b='$b')"
             }
         }
 
@@ -21,7 +45,11 @@ class Day24 {
                 val parts = line.split(" ")
                 return when (val cmd = parts[0]) {
                     "inp" -> Inp(parts[1])
-                    "mul" -> Mul(parts[1], parts[2].toInt())
+                    "mul" -> Mul(parts[1], parts[2])
+                    "eql" -> Eql(parts[1], parts[2])
+                    "add" -> Add(parts[1], parts[2])
+                    "div" -> Div(parts[1], parts[2])
+                    "mod" -> Mod(parts[1], parts[2])
                     else -> throw IllegalArgumentException("Unknown command $cmd")
                 }
             }
@@ -34,29 +62,57 @@ class Day24 {
 
     fun part1() {
         val commands = Command.parse(File("day24.txt").readLines())
-        commands.debug("Parsed input")
+        // commands.debug("Parsed input")
 
-        val result = run("91234567890", commands)
+        separator(description = "Running program")
+        val result = run("13579246899999", commands)
         for ((k, v) in result) {
             println("$k=$v")
         }
     }
 
     fun run(input: String, commands: List<Command>): Map<String, Int> {
-        val mem = mutableMapOf<String, Int>()
+        val mem = mutableMapOf(
+            "w" to 0,
+            "x" to 0,
+            "y" to 0,
+            "z" to 0,
+        )
         var inputIdx = 0
         for (c in commands) {
+            // println("Executing $c       ${mem}")
             when (c) {
                 is Command.Inp -> {
                     mem[c.dest] = "${input[inputIdx++]}".toInt()
                 }
                 is Command.Mul -> {
-                    mem[c.a] = mem[c.a]!! * c.b
+                    mem[c.a] = mem[c.a]!! * (toNumber(c.b) ?: mem[c.b]!!)
                 }
+                is Command.Eql -> {
+                    mem[c.a] = if (mem[c.a]!! == (toNumber(c.b) ?: mem[c.b]!!)) 1 else 0
+                }
+                is Command.Add -> {
+                    mem[c.a] = mem[c.a]!! + (toNumber(c.b) ?: mem[c.b]!!)!!
+                }
+                is Command.Div -> {
+                    mem[c.a] = mem[c.a]!! / (toNumber(c.b) ?: mem[c.b]!!)
+                }
+                is Command.Mod -> {
+                    mem[c.a] = mem[c.a]!! % (toNumber(c.b) ?: mem[c.b]!!)
+                }
+
                 else -> throw IllegalArgumentException("Unknown command $c")
             }
         }
 
         return mem.toMap()
+    }
+
+    private fun toNumber(s: String): Int? {
+        try {
+            return s.toInt()
+        } catch (e: NumberFormatException) {
+            return null
+        }
     }
 }
